1ba2e215eb8f4ab20fee63c406bd7773517ec657
DandyLikeAndy <andy.dashin@gmail.com>
2018-08-30 21:50:43 +0300

    30.08.18 set and fix vars

--- app/blocks/common.blocks/simple-slider/simple-slider.js
+++ app/blocks/common.blocks/simple-slider/simple-slider.js
@@ -26,23 +26,34 @@ const ClassNameEl = {
 class SimpleSlider {
 
     constructor(opts) {
-        const slEl = opts.triggerEl,
-            slItems = slEl.getElementsByClassName(ClassNameEl.ITEM),
-            slInfoBlocks = slEl.getElementsByClassName(ClassNameEl.INFO);
-            buttonPlay = slEl.getElementsByClassName(ClassNameEl.BUTTON_PLAY);
+
+        
+        this.slEl = opts.slEl;
+        this.slItems = slEl.getElementsByClassName(ClassNameEl.ITEM);
+        this.slInfoBlocks = slEl.getElementsByClassName(ClassNameEl.INFO),
+        this.buttonPlay = slEl.getElementsByClassName(ClassNameEl.BUTTON_PLAY),
+            
+
+        this._currentImgNum = this.slItems.length - 1; //it is number of top element item  in slider
+        this._isWork = null;
+        this._timerAutoPlayID = null;
+        this._timerLoadID = null;
 
 
-            //todo move in this or closure
-        let currentImgNum = slItems.length - 1, //it is number of top element item  in slider
+        this._initElms();
+        this._initTouchEvent();
+        if(opts.isAutoPlay) toggleAutoPlay(); //todo: to fix autoPlay(true)
+
+
+
+        /* let currentImgNum = slItems.length - 1, //it is number of top element item  in slider
             dir = null,
             isWork = null,
             isAnimationInfo = null,
             timerAutoPlayID = null,
             timerLoadID = null,
             currentItem = null,
-            nextInfoBlock = null;
-
-        this.slEl = opts.triggerEl;
+            nextInfoBlock = null; */
     }
 }
 
