fdcb365a961fcb1d2457eb7eb2e10cbf2b97543b
DandyLikeAndy <andy.dashin@gmail.com>
2018-08-31 21:22:39 +0300

    31.08.18 Write private methos

--- app/blocks/common.blocks/simple-slider/simple-slider.js
+++ app/blocks/common.blocks/simple-slider/simple-slider.js
@@ -6,8 +6,13 @@ const CLASS_SL_ELEM = 'simpleSlider';
 
 const ClassNameEl = {
     ITEM: `${CLASS_SL_ELEM}__item`,
+    IMG: `${CLASS_SL_ELEM}__img`,
     INFO: `${CLASS_SL_ELEM}__info`,
     BUTTON_PLAY: `${CLASS_SL_ELEM}__button_play-pause`
+};
+
+const Attribute = {
+    DATA_IMG_SRC: 'data-img-realsrc'
 };
 
 
@@ -23,43 +28,111 @@ const ClassNameEl = {
 // Class Definition
 // ------------------------------------------------------------------------
 
-class SimpleSlider {
+class SimSlider {
 
     constructor(opts) {
 
-        
         this.slEl = opts.slEl;
         this.slItems = slEl.getElementsByClassName(ClassNameEl.ITEM);
-        this.slInfoBlocks = slEl.getElementsByClassName(ClassNameEl.INFO),
-        this.buttonPlay = slEl.getElementsByClassName(ClassNameEl.BUTTON_PLAY),
-            
+        this.slInfoBlocks = slEl.getElementsByClassName(ClassNameEl.INFO);
+        this.buttonPlay = slEl.getElementsByClassName(ClassNameEl.BUTTON_PLAY);
 
-        this._currentImgNum = this.slItems.length - 1; //it is number of top element item  in slider
+        this._currentItemNum = this.slItems.length - 1; //*** !!! it is number of top element item  in slider
+        this._currentItem = slItems[this._currentItemNum]; //*** !!!
         this._isWork = null;
+        this._isAutoPlayOn = null;
         this._timerAutoPlayID = null;
         this._timerLoadID = null;
-
+        this._qtPreloaded = opts.qtPreloaded || 1;
 
         this._initElms();
         this._initTouchEvent();
-        if(opts.isAutoPlay) toggleAutoPlay(); //todo: to fix autoPlay(true)
+        if(opts.isAutoPlay) this.toggleAutoPlay(); //todo: to fix autoPlay(true)
 
-
-
-        /* let currentImgNum = slItems.length - 1, //it is number of top element item  in slider
-            dir = null,
-            isWork = null,
-            isAnimationInfo = null,
-            timerAutoPlayID = null,
-            timerLoadID = null,
-            currentItem = null,
-            nextInfoBlock = null; */
     }
+
+
+// ***************************************
+// Private methods
+// ***************************************
+
+_initElms() {
+    const currentItem = this._currentItem,
+        firstImg = SimSlider._getImg(currentItem);
+
+    this._loadImg(firstImg); //load first img
+    currentItem.cssText = "'zIndex': 1; 'will-change': 'transform'"; //todo: для изображения или + transform??
+    this._preLoadImgs(currentImgNum); //загружаем остальные фотографии (которые должны подгрузиться в данный момент времени)
+
+    this.slEl.onclick = this.onClick;
+}
+
+
+_loadImg(img) {
+
+    const src = img.getAttribute(Attribute.DATA_IMG_SRC),
+        imgLoad = document.createElement('img');
+
+    if (!src) { //если загружено, просто показываем - уже как фоновое изображение
+        img.style.opacity = 1;
+        img.isloaded = true;
+        return;
+    }
+
+    imgLoad.onload = onLoad;
+    imgLoad.src = src;
+
+    function onLoad() {
+        img.style.backgroundImage = "url(" + src + ")";
+        img.setAttribute(Attribute.DATA_IMG_SRC, '');
+        img.style.willChange = 'opacity'; //*** !!!
+        img.style.opacity = 1;
+        img.isloaded = true;
+    }
+}
+
+
+_preLoadImgs(itemNum) {
+    const qtPreloaded = this._qtPreloaded,
+        slItems = this.slItems;
+
+        if ( qtPreloaded > slItems.length-1 ) qtPreloaded = slItems.length-1;
+        for ( let i = itemNum-qtPreloaded; i <= itemNum+qtPreloaded; i++ ) {
+            if (i===itemNum) continue; // т.к.текущ. изобр загружено
+
+            let item = slItems[i],
+                img = item && SimSlider._getImg(slItems[i]);
+
+            if (!item && i<0) {
+                img = SimSlider._getImg(slItems[slItems.length + i]);
+            } else if (!item && i>0) {
+                img = SimSlider._getImg(slItems[i - slItems.length]);
+            }
+
+            if (img.isloaded) continue;
+            loadImg(img);
+        }
+}
+_onClick(e) {}
+_initTouchEvent();
+
+
+// ***************************************
+// Static methods
+// ***************************************
+static _getImg(el) {
+    el.getElementsByClassName(ClassNameEl.IMG)[0];
+}
+
+
 }
 
 
 // ------------------------------------------------------------------------
 // Initialization 
 // ------------------------------------------------------------------------
+
+
+
 
 });
