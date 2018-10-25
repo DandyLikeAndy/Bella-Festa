f3dc978912bf323de661d3e63a901899c10f30c4
DandyLikeAndy <andy.dashin@gmail.com>
2018-10-25 14:59:32 +0300

    25.10.18 fix _stSlideAnimate(), _onClick(e)

--- app/blocks/common.blocks/simple-slider/simple-slider.js
+++ app/blocks/common.blocks/simple-slider/simple-slider.js
@@ -43,12 +43,12 @@ class SimSlider {
         this.slInfoBlocks = slEl.getElementsByClassName(ClassNameEl.INFO);
         this.buttonPlayIco = slEl.querySelector(ClassNameEl.BUTTON_PLAY + ' ' + ClassNameEl.BUTTON_ICO);
 
-        //save instanse in element
+        //save instance in element
         this.slEl._simSlider = this;
 
         this._currentItemNum = this.slItems.length - 1; //*** !!! it is number of top element item  in slider
-        this._nextItemNum = null,
-        this._currentItem = slItems[this._currentItemNum]; //*** !!!
+        this._nextItemNum = null;
+        this._currentItem = this.slItems[this._currentItemNum]; //*** !!!
         this._isWork = null;
         this._isAutoPlayOn = null;
         this._timerAutoPlayID = null;
@@ -110,116 +110,119 @@ class SimSlider {
 // Private methods
 // ***************************************
 
-_initElms() {
-    const currentItem = this._currentItem,
-        firstImg = SimSlider._getImg(currentItem);
-
-    this._loadImg(firstImg); //load first img
-    currentItem.cssText = "zIndex: 1; will-change: transform"; //todo: для изображения или + transform??
-    this._preLoadImgs(currentImgNum); //загружаем остальные фотографии (которые должны подгрузиться в данный момент времени)
-
-    this.slEl.onclick = this._onClick;
-}
+    _initElms() {
+        const currentItem = this._currentItem,
+            firstImg = SimSlider._getImg(currentItem),
+            $slEl = ElU(slEl);
 
 
-_loadImg(img) {
+        this._loadImg(firstImg); //load first img
+        currentItem.cssText = "zIndex: 1; will-change: transform"; //todo: для изображения или + transform??
+        this._preLoadImgs(this._currentItemNum); //загружаем остальные фотографии (которые должны подгрузиться в данный момент времени)
 
-    const src = img.getAttribute(Attribute.DATA_IMG_SRC),
-        imgLoad = document.createElement('img');
-
-    if (!src) { //если загружено, просто показываем - уже как фоновое изображение
-        img.style.opacity = 1;
-        img.isloaded = true;
-        return;
+        $slEl.on('click', this._onClick(), this);
     }
 
-    imgLoad.onload = onLoad;
-    imgLoad.src = src;
 
-    function onLoad() {
-        img.style.backgroundImage = "url(" + src + ")";
-        img.setAttribute(Attribute.DATA_IMG_SRC, '');
-        img.style.willChange = 'opacity'; //*** !!!
-        img.style.opacity = 1;
-        img.isloaded = true;
+    _loadImg(img) {
+
+        const src = img.getAttribute(Attribute.DATA_IMG_SRC),
+            imgLoad = document.createElement('img');
+
+        if (!src) { //если загружено, просто показываем - уже как фоновое изображение
+            img.style.opacity = 1;
+            img.isloaded = true;
+            return;
+        }
+
+        imgLoad.onload = onLoad;
+        imgLoad.src = src;
+
+        function onLoad() {
+            img.style.backgroundImage = "url(" + src + ")";
+            img.setAttribute(Attribute.DATA_IMG_SRC, '');
+            img.style.willChange = 'opacity'; //*** !!!
+            img.style.opacity = 1;
+            img.isloaded = true;
+        }
     }
-}
 
 
-_preLoadImgs(itemNum) {
-    let qtPreloaded = this._qtPreloaded;
-    const  slItems = this.slItems;
+    _preLoadImgs(itemNum) {
+        let qtPreloaded = this._qtPreloaded;
+        const slItems = this.slItems;
 
-        if ( qtPreloaded > slItems.length-1 ) qtPreloaded = slItems.length-1;
-        for ( let i = itemNum-qtPreloaded; i <= itemNum+qtPreloaded; i++ ) {
-            if (i===itemNum) continue; // т.к.текущ. изобр загружено
+        if (qtPreloaded > slItems.length - 1) qtPreloaded = slItems.length - 1;
+        for (let i = itemNum - qtPreloaded; i <= itemNum + qtPreloaded; i++) {
+            if (i === itemNum) continue; // т.к.текущ. изобр загружено
 
             let item = slItems[i],
                 img = item && SimSlider._getImg(slItems[i]);
 
-            if (!item && i<0) {
+            if (!item && i < 0) {
                 img = SimSlider._getImg(slItems[slItems.length + i]);
-            } else if (!item && i>0) {
+            } else if (!item && i > 0) {
                 img = SimSlider._getImg(slItems[i - slItems.length]);
             }
 
             if (img.isloaded) continue;
             loadImg(img);
         }
-}
-
-
-
-_onClick(e) {
-    if (e.target.closest(ClassNameEl.BUTTON_PLAY)) {
-        toggleAutoPlay();
-        return;
-
-    } else if (this._isWork) {
-        return;
-
-    } else if ($(e.target).closest(ClassNameEl.BUTTON_NEXT)) {
-        dir = 1;
-        go(dir);
-
-    } else if ($(e.target).closest(ClassNameEl.BUTTON_PREV)) {
-        dir = -1;
-        go(dir);
     }
-}
 
-_initTouchEvent();
 
-_getNextItemNum(dir) {
-    const curNum = this._currentItemNum,
-        slItems = this.slItems;
+    _onClick(e) {
+        if (e.target.closest(ClassNameEl.BUTTON_PLAY)) {
+            this.toggleAutoPlay();
+            return;
 
-    let nextItemNum = null;    
+        } else if (this._isWork) {
+            return;
 
-    if(dir == -1) {
-        return nextItemNum = curNum >= slItems.length - 1 ? 0 : curNum + 1;
-    } else {
-        return nextItemNum = curNum === 0 ? slItems.length - 1 : curNum - 1;
+        } else if (e.target.closest(ClassNameEl.BUTTON_NEXT)) {
+            let dir = 1;
+            this.go(dir);
+
+        } else if (e.target.closest(ClassNameEl.BUTTON_PREV)) {
+            let dir = -1;
+            this.go(dir);
+        }
     }
-}
 
-_setClassButtonPlayIco(action) {
-    const button = this.buttonPlayIco;
+    _initTouchEvent() {
+    };
 
-    if (button && action === 'pause') {
-        button.classList.remove(StatusClassName.ICO_PLAY);
-        button.classList.add(StatusClassName.ICO_PAUSE);
-    } else if (button && action === 'play') {
-        button.classList.remove(StatusClassName.ICO_PAUSE);
-        button.classList.add(StatusClassName.ICO_PLAY);
+    _getNextItemNum(dir) {
+        const curNum = this._currentItemNum,
+            slItems = this.slItems;
+
+        let nextItemNum = null;
+
+        if (dir === -1) {
+            return nextItemNum = curNum >= slItems.length - 1 ? 0 : curNum + 1;
+        } else {
+            return nextItemNum = curNum === 0 ? slItems.length - 1 : curNum - 1;
+        }
     }
-}
 
-_setAnimateFunc(typeAnimation) {
-    let typeAn = typeAnimation || 'fade',
-        stSlideAnimation = SimSlider._stSlideAnimate[typeAn];
+    _setClassButtonPlayIco(action) {
+        const button = this.buttonPlayIco;
+
+        if (button && action === 'pause') {
+            button.classList.remove(StatusClassName.ICO_PLAY);
+            button.classList.add(StatusClassName.ICO_PAUSE);
+        } else if (button && action === 'play') {
+            button.classList.remove(StatusClassName.ICO_PAUSE);
+            button.classList.add(StatusClassName.ICO_PLAY);
+        }
+    }
+
+    _setAnimateFunc(typeAnimation) {
+        let typeAn = typeAnimation || 'fade',
+            stSlideAnimation = SimSlider._stSlideAnimate[typeAn];
+
         this._animateMove = stSlideAnimation instanceof Function ? stSlideAnimation : SimSlider._stSlideAnimate.fade;
-}
+    }
 
 
 // ***************************************
@@ -230,45 +233,45 @@ _setAnimateFunc(typeAnimation) {
 //call OnAutoPlay();
 //set currentItemNum = nextItemNum
 //set currentItem = nextAnimEl;
-static get _stSlideAnimate() {
-    return {
-        fade: function() { //?????
-            const animEl = this._currentItem,
-                nextAnimEl = this.slItems[this._nextItemNum],
-                $animEl = ElU(this._currentItem);
-        
-            item._$el = $animEl;
-        
-            $item.on('transitionend.fade', transitionComplete, this);   
-            item.style.opacity = 0;
-        
-            function transitionComplete(e) {
+    static get _stSlideAnimate() {
+        return {
+            fade: function () { //?????
                 const animEl = this._currentItem,
-                    $animEl = animEl._$el;
-        
-                if (e.target === animEl && e.propertyName != 'opacity') return;
-                $animEl.off('transitionend.fade');
-        
-                //reset style for current item
-                animEl.cssText('Z-index: 0; opacity: 0; will-change: ""');
-                //preparation transition for next item
-                this.slItems[this._nextItemNum].style.willChange = "opacity";
-        
-                this._currentItemNum = this._nextItemNum;
-                this._currentItem = nextAnimEl;
-                this.isWork = false;
-        
-                if(this.isAutoPlay) this.onAutoPlay();
+                    nextAnimEl = this.slItems[this._nextItemNum],
+                    $animEl = ElU(this._currentItem);
+
+                item._$el = ElU(animEl);
+
+                $animEl.on('transitionend.fade', transitionComplete, this);
+                item.style.opacity = 0;
+
+                function transitionComplete(e) {
+                    const animEl = this._currentItem,
+                        $animEl = animEl._$el;
+
+                    if (e.target === animEl && e.propertyName != 'opacity') return;
+                    $animEl.off('transitionend.fade');
+
+                    //reset style for current item
+                    animEl.cssText('Z-index: 0; opacity: 0; will-change: ""');
+                    //preparation transition for next item
+                    this.slItems[this._nextItemNum].style.willChange = "opacity";
+
+                    this._currentItemNum = this._nextItemNum;
+                    this._currentItem = nextAnimEl;
+                    this.isWork = false;
+
+                    if (this.isAutoPlay) this.onAutoPlay();
+                }
+
             }
-        
         }
     }
-}
 
 
 
 static _getImg(el) {
-    el.getElementsByClassName(ClassNameEl.IMG)[0];
+     return el.getElementsByClassName(ClassNameEl.IMG)[0];
 }
 
 
